e1f102513cc0ca3951e099b8e3727736e5f3cde9
Nataly Suvorova <suvorova.nata.a@gmail.com>
2023-12-28 15:35:40 +0300

    3

--- Dz3/product.js
+++ Dz3/product.js
@@ -2,7 +2,7 @@
 const changePageEl = document.querySelector(".changePage");
 const contentEl = document.querySelector(".content");
 
-
+// массив отзывов из LocalStorage
 const feedbackList = productFromStorage();
 
 // вывод списка отзывов на страницу
@@ -10,7 +10,9 @@ const contentHtml = feedbackList
   .map(
     (el) => `<article>
                <h1>${el.nameProduct}</h1>
-               <ul class="feedbackList" style="display: none;">${el.feedback.map((i) => `<li>${i}</li>`).join(" ")}</ul>
+               <ul class="feedbackList" style="display: none;">${el.feedback
+                 .map((i) => `<li>${i}</li>`)
+                 .join(" ")}</ul>
                <button class="feedbackList__btn">Показать отзывы</button>
             </article>`
   )
@@ -18,38 +20,23 @@ const contentHtml = feedbackList
 
 contentEl.innerHTML = contentHtml;
 
-const feedbackListEl = document.querySelectorAll(".feedbackList")
-const feedbackListBtnEl = document.querySelectorAll(".feedbackList__btn")
+const feedbackListEl = document.querySelectorAll(".feedbackList");
+const feedbackListBtnEl = document.querySelectorAll(".feedbackList__btn");
 
-
-function showFeedback() {
-  if (feedbackListEl.style.display === "none") {
-    feedbackListEl.style.display = "block";
-    feedbackListBtnEl.innerHTML = "Скрыть отзывы"
-  } else {
-    feedbackListEl.style.display = "none";
-    feedbackListBtnEl.innerHTML = "Показать отзывы"
-  }
-}
-
-for (let i=0; i < feedbackListBtnEl.length; i++) {
-  console.log(feedbackListBtnEl[i]);
-  console.log(feedbackListEl[i]);
-  feedbackListBtnEl[i].addEventListener('click', () => {
+// обработка события click (показывает или скрывает отзывы на конкретный товар)
+for (let i = 0; i < feedbackListBtnEl.length; i++) {
+  feedbackListBtnEl[i].addEventListener("click", () => {
     if (feedbackListEl[i].style.display === "none") {
       feedbackListEl[i].style.display = "block";
-      feedbackListBtnEl[i].innerHTML = "Скрыть отзывы"
+      feedbackListBtnEl[i].innerHTML = "Скрыть отзывы";
     } else {
       feedbackListEl[i].style.display = "none";
-      feedbackListBtnEl[i].innerHTML = "Показать отзывы"
+      feedbackListBtnEl[i].innerHTML = "Показать отзывы";
     }
   });
 }
 
-
-
-
-
+// переход на страницу добавления отзывов
 function changePage() {
   window.open("addFeedback.html", "_self");
 }
